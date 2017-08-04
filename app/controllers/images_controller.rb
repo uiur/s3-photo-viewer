d959be0b2893f789ccd2933556e1da82ad94c6ef
Kazato Sugimoto <kazato-sugimoto@cookpad.com>
2017-08-04 13:48:14 +0900

    Support page=10

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,14 @@
 class ImagesController < ApplicationController
+  DEFAULT_SIZE = 10
+
   def index
     s3 = Aws::S3::Resource.new
     bucket = s3.bucket(params[:bucket])
-    @objs = bucket.objects(prefix: params[:prefix])
+
+    @objs =
+      bucket
+        .objects(prefix: params[:prefix])
+        .reverse_each
+        .first(params[:size].to_i || DEFAULT_SIZE)
   end
 end
