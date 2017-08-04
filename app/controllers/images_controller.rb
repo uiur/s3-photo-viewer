aba23a76f9bb4831c9debf1a8fe5c6ce346ac24e
Kazato Sugimoto <kazato-sugimoto@cookpad.com>
2017-08-04 14:08:59 +0900

    Use kaminari

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -11,10 +11,12 @@ class ImagesController < ApplicationController
       @objs = @objs.reverse_each
     end
 
+    @objs = @objs.select { |obj| is_image?(obj.key) }
+
     page = params[:page]&.to_i || 1
     size = params[:size]&.to_i || DEFAULT_SIZE
 
-    @objs = @objs.select { |obj| is_image?(obj.key) }.drop((page - 1) * size).first(size)
+    @objs = Kaminari.paginate_array(@objs).page(page).per(size)
   end
 
   private
